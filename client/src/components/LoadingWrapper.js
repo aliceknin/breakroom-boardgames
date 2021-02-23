1b19abfb7d2ede2d4ec876f758d7a4fcda06d94b
Alice Nin <aliceknin@gmail.com>
2021-02-22 16:53:03 -0800

    prettier loading state

--- client/src/components/LoadingWrapper.js
+++ client/src/components/LoadingWrapper.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "../styles/LoadingWrapper.scss";
 
 const LoadingWrapper = ({ isLoading, children }) => {
-  return isLoading ? <div>Loading...</div> : children;
+  return isLoading ? (
+    <div className="loading">
+      <p>
+        Loading
+        <span>.</span>
+        <span>.</span>
+        <span>.</span>
+      </p>
+    </div>
+  ) : (
+    children
+  );
 };
 
 export default LoadingWrapper;
