e58504b34406ee78defeba02c85c5f7654b88771
Alice Nin <aliceknin@gmail.com>
2021-02-22 22:17:23 -0800

    min board size, my focus styles > outlines, swallow handled click

--- client/src/components/Overlay.js
+++ client/src/components/Overlay.js
@@ -4,10 +4,15 @@ import "../styles/Overlay.scss";
 const Overlay = ({ children, className }) => {
   const [closed, setClosed] = useState(false);
 
+  function handleClick(e) {
+    e.stopPropagation();
+    setClosed(true);
+  }
+
   return (
     !closed && (
       <div className={"game-overlay " + className}>
-        <button className="close" onClick={() => setClosed(true)}>
+        <button className="close" onClick={handleClick}>
           X
         </button>
         {children}
