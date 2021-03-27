e59a984208f6895e86a9fc0531f7b995a4efdab8
Alice Nin <aliceknin@gmail.com>
2021-03-27 03:14:29 -0700

    first pass at copy link button, better info icon css

--- client/src/components/RoomHeader.js
+++ client/src/components/RoomHeader.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 
 const RoomHeader = ({ socket, roomName }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [copied, setCopied] = useState(false);
+
+  function copyURL() {
+    console.log("trying to copy " + window.location);
+    navigator.clipboard.writeText(window.location);
+    setCopied(true);
+  }
 
   return (
     <header>
@@ -10,12 +17,32 @@ const RoomHeader = ({ socket, roomName }) => {
           Breakroom Boardgames
         </a>
       </div>
+      {copied && (
+        <div className="alert copied">
+          Copied!{" "}
+          <button className="close" onClick={() => setCopied(false)}>
+            <i className="close"></i>
+          </button>
+        </div>
+      )}
       <button onClick={() => setDropdownOpen((o) => !o)} className="menu">
         <i className="fas fa-info"></i>
       </button>
       <div className={"dropdown-content" + (dropdownOpen ? " open" : "")}>
         <div className="room-name">
           Game Room: <span>{roomName}</span>
+          <button onClick={copyURL} className="copy-url">
+            {/* <i
+              className="far fa-clipboard"
+              aria-label="Copy the room link to share with your friends!"
+              data-label-popup
+            ></i> */}
+            <i
+              className="fas fa-link"
+              aria-label="Copy the room link to share with your friends!"
+              data-label-popup
+            ></i>
+          </button>
         </div>
         <div className="username">
           Hi{socket?.userName && ", " + socket.userName}!
